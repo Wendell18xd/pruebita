d381fec4912adcbada8ec5dcf3b22a0f2584dcd7
Wendell Smith Vidal <wendell18cuenta@gmail.com>
2020-12-01 20:07:44 -0500

    GA

--- script.js
+++
@@ -1,5 +0,0 @@
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('./sw.js')
-        .then(reg => console.log('registro de SW exitoso', reg))
-        .catch(err => console.warn('Error al tratar de registrar el sw', err))
-}
